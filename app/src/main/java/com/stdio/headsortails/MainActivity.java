7ebbf7c3c6551ae450f37371148a4af0bb67a866
Std1o <37378410+Std1o@users.noreply.github.com>
2020-11-15 00:31:23 +0300

    start menu

--- app/src/main/java/com/stdio/headsortails/MainActivity.java
+++ app/src/main/java/com/stdio/headsortails/MainActivity.java
@@ -3,10 +3,14 @@ package com.stdio.headsortails;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout hud, start_menu, shadow_for_start_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        hud = findViewById(R.id.hud);
+        start_menu = findViewById(R.id.start_menu);
+        shadow_for_start_menu = findViewById(R.id.shadow_for_start_menu);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStart:
+                hud.setVisibility(View.VISIBLE);
+                start_menu.setVisibility(View.GONE);
+                shadow_for_start_menu.setVisibility(View.GONE);
+                break;
+        }
     }
 }
