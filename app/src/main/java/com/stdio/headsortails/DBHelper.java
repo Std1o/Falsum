d46ee282e4f38b1ace6821ccad32b7396648df28
Std1o <37378410+Std1o@users.noreply.github.com>
2020-11-17 00:13:03 +0300

    writing and reading database sqlite

---
+++ app/src/main/java/com/stdio/headsortails/DBHelper.java
@@ -0,0 +1,44 @@
+package com.stdio.headsortails;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper  extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "configurationsDb";
+    public static final String TABLE_CONFIGURATIONS = "configurations";
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_BALANCE = "balance";
+    public static final String KEY_BILL = "bill";
+    public static final String KEY_BILL_DATE = "bill_date";
+    public static final String KEY_BILL_TIME = "bill_time";
+    public static final String KEY_REWARD = "reward";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_TIME = "time";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_CONFIGURATIONS + "(" + KEY_ID
+                + " integer primary key,"  + KEY_NAME + " text," +  KEY_BALANCE
+                + " text," + KEY_BILL + " text," + KEY_BILL_DATE + " text," + KEY_BILL_TIME + " text," + KEY_REWARD
+                + " text," + KEY_DATE + " text," + KEY_TIME + " text" + ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_CONFIGURATIONS);
+
+        onCreate(db);
+
+    }
+}
--- app/src/main/java/com/stdio/headsortails/MainActivity.java
+++ app/src/main/java/com/stdio/headsortails/MainActivity.java
@@ -2,24 +2,49 @@ package com.stdio.headsortails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     LinearLayout hud, start_menu, shadow_for_start_menu, input_name;
+    EditText etName;
+    TextView tvName;
+    DBHelper dbHelper;
+    SQLiteDatabase database;
+    SimpleDateFormat sdfDate = new SimpleDateFormat("dd.MM.yyyy");
+    SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm");
+    Date currentDateTime = Calendar.getInstance().getTime();
+    String name = "";
+    int balance = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFullScreen();
+        setContentView(R.layout.activity_main);
+        initViews();
+        dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+        getData();
+        tvName.setText(name);
+    }
+
+    private void setFullScreen() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-        initViews();
     }
 
     private void initViews() {
@@ -27,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         start_menu = findViewById(R.id.start_menu);
         shadow_for_start_menu = findViewById(R.id.shadow_for_start_menu);
         input_name = findViewById(R.id.input_name);
+        etName = findViewById(R.id.etName);
+        tvName = findViewById(R.id.tvName);
     }
 
     public void onClick(View v) {
@@ -39,7 +66,35 @@ public class MainActivity extends AppCompatActivity {
                 hud.setVisibility(View.VISIBLE);
                 input_name.setVisibility(View.GONE);
                 shadow_for_start_menu.setVisibility(View.GONE);
+                saveInitialConfigurations();
                 break;
+        }
+    }
+
+    private void saveInitialConfigurations() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBHelper.KEY_NAME, etName.getText().toString());
+        contentValues.put(DBHelper.KEY_DATE, sdfDate.format(currentDateTime));
+        contentValues.put(DBHelper.KEY_TIME, sdfTime.format(currentDateTime));
+        contentValues.put(DBHelper.KEY_BALANCE, 1000);
+        contentValues.put(DBHelper.KEY_BILL, 0);
+        contentValues.put(DBHelper.KEY_BILL_DATE, "-");
+        contentValues.put(DBHelper.KEY_BILL_TIME, "-");
+        contentValues.put(DBHelper.KEY_REWARD, 0);
+        database.insert(DBHelper.TABLE_CONFIGURATIONS, null, contentValues);
+    }
+
+    private void getData() {
+        Cursor cursor = database.query(DBHelper.TABLE_CONFIGURATIONS, null, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+            int balanceIndex = cursor.getColumnIndex(DBHelper.KEY_BALANCE);
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            name = cursor.getString(nameIndex);
+            balance = cursor.getInt(balanceIndex);
+        } else {
+            cursor.close();
         }
     }
 }
